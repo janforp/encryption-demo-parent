aa867e2345fc6a7403e8d09062c55b1f5417be59
Janita <zhuchenjian@huizhi-tech.com>
2017-03-31 18:05:44 +0800

    md5，rsa，base64，aes

--- aes/src/main/java/com/janita/aes/AesApplication.java
+++ aes/src/main/java/com/janita/aes/AesApplication.java
@@ -1,12 +1,19 @@
 package com.janita.aes;
 
+import com.janita.aes.util.AES;
+
 /**
  * Created by Janita on 2017-03-31 17:55
  */
 public class AesApplication {
 
-    public static void main(String[] args){
+    static AES aes = new AES();
 
+    public static void main(String[] args){
+        String msg = "大家好";
+        String enc = aes.encryptAES(msg);
+        System.out.println("*******"+enc);
+        System.out.println("*******"+aes.decryptAES(enc));
 
     }
 }
